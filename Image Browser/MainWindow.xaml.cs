8fd3d143b82c1b15cf9a2e9e83a85a8f97ac0228
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-05-20 11:36:26 +0200

    add thumbnail size slider with data binding

--- Image Browser/MainWindow.xaml.cs
+++ Image Browser/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,17 +14,32 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Image_Browser.Annotations;
 
 namespace Image_Browser
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private int _thumbnailSize = 50;
+
+        public int ThumbnailSize
+        {
+            get { return _thumbnailSize; }
+            set
+            {
+                if (value == _thumbnailSize) return;
+                _thumbnailSize = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
         private void OpenImage(object sender, RoutedEventArgs e)
@@ -43,6 +60,14 @@ namespace Image_Browser
         private void DisplayAbout(object sender, RoutedEventArgs e)
         {
             MessageBox.Show(this, "Simple image browser", "About", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
